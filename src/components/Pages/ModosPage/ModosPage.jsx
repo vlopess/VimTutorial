07e3b1530f3898a8677b278cf0ff049bbc2561a7
victor <victorlopes9291@gmail.com>
2024-10-01 22:16:51 -0300

    feat: finalizando a page de modos

--- src/components/Pages/ModosPage/ModosPage.jsx
+++ src/components/Pages/ModosPage/ModosPage.jsx
@@ -1,5 +1,7 @@
 import "./ModosPage.css";
 import {Key} from "../../utils/Key/Key.jsx";
+import {NewFileTerminal} from "../../utils/PromptCard/Custom/NewFileTerminal.jsx";
+import {VisualModeTerminal} from "../../utils/PromptCard/Custom/VisualModeTerminal.jsx";
 
 export const ModosPage = () => {
     return (
@@ -15,9 +17,15 @@ export const ModosPage = () => {
                 <p>O Vim suporta alguns modos de edição, sendo cada um deles usados para tarefas específicas. Os modos
                     mais usados são <i>normal, inserção, visual e comando.</i></p>
                 <div style={{display: "flex", alignItems: "center", flexDirection: "column"}}>
-                    <img height={350} width={350}
-                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Vim_modes.svg/800px-Vim_modes.svg.png"
-                         alt="Vim Modes Image"/>
+                    <img
+                        style={{
+                            margin: "5px",
+                            padding: "15px",
+                            backgroundColor: "grey"
+                        }}
+                        height={350} width={350}
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Vim_modes.svg/800px-Vim_modes.svg.png"
+                        alt="Vim Modes Image"/>
                     <p style={{
                         fontSize: 12,
                         textAlign: "center",
@@ -32,29 +40,37 @@ export const ModosPage = () => {
                     O modo padrão para navegar e manipular texto. Quando acessa o Vim você está nele, e é
                     a partir dele que vocẽ navega para os outros modos. É aqui que você move o cursor, copia,
                     cola e apaga texto, entre outras ações. Boa partes dos caracteres são interpretados como comando.
-                    Aqui estão os principais comandos que podem ser usados no Modo Normal:
+                    Aqui estão os principais comandos de cópia, corte e colagem que podem ser usados no Modo Normal:
                 </p>
                 <table>
                     <tr>
                         <th>Comando</th>
                         <th>Descrição</th>
                     </tr>
-                    {/*<tr>*/}
-                    {/*    <td><Key text={"W"}/></td>*/}
-                    {/*    <td>Move para o início da próxima palavra.</td>*/}
-                    {/*</tr>*/}
-                    {/*<tr>*/}
-                    {/*    <td><Key text={"E"}/></td>*/}
-                    {/*    <td>Move para o final da palavra atual.</td>*/}
-                    {/*</tr>*/}
-                    {/*<tr>*/}
-                    {/*    <td><Key text={"0"}/></td>*/}
-                    {/*    <td>Move para o início da linha.</td>*/}
-                    {/*</tr>*/}
-                    {/*<tr>*/}
-                    {/*    <td><Key text={"Shift"}/> + <Key text={"4"}/> = ($)</td>*/}
-                    {/*    <td>Move para o final da linha.</td>*/}
-                    {/*</tr>*/}
+                    {/*{/<tr>/}*/}
+                    {/*/!*    <td><Key text={"W"}/></td>*!/*/}
+                    {/*/!*    <td>Move para o início da próxima palavra.</td>*!/*/}
+                    {/*{/</tr>/}*/}
+                    {/*{/<tr>/}*/}
+                    {/*/!*    <td><Key text={"E"}/></td>*!/*/}
+                    {/*/!*    <td>Move para o final da palavra atual.</td>*!/*/}
+                    {/*{/</tr>/}*/}
+                    {/*{/<tr>/}*/}
+                    {/*/!*    <td><Key text={"0"}/></td>*!/*/}
+                    {/*/!*    <td>Move para o início da linha.</td>*!/*/}
+                    {/*{/</tr>/}*/}
+                    {/*{/<tr>/}*/}
+                    {/*/!*    <td><Key text={"Shift"}/> + <Key text={"4"}/> = ($)</td>*!/*/}
+                    {/*/!*    <td>Move para o final da linha.</td>*!/*/}
+                    {/*{/</tr>/}*/}
+                    {/*{/<tr>/}*/}
+                    {/*/!*    <td><Key text={"C"}/><Key text={"C"}/></td>*!/*/}
+                    {/*/!*    <td>Apaga a linha atual e entra no modo de inserção.</td>*!/*/}
+                    {/*{/</tr>/}*/}
+                    {/*{/<tr>/}*/}
+                    {/*/!*    <td><Key text={"U"}/></td>*!/*/}
+                    {/*/!*    <td>(Undo), o equilvante ao CTRL+Z.</td>*!/*/}
+                    {/*{/</tr>/}*/}
                     <tr>
                         <td><Key text={"Y"}/></td>
                         <td>Copia (yank) a linha atual. O equilvante ao CTRL+C</td>
@@ -64,44 +80,259 @@ export const ModosPage = () => {
                         <td>Copia da posição atual até o início da próxima palavra.</td>
                     </tr>
                     <tr>
-                        <td><Key text={"Y"}/><Key text={"Shift"}/><Key text={"$"}/></td>
+                        <td><Key text={"Y"}/><Key text={"$"}/></td>
                         <td>Copia da posição atual até o final da linha.</td>
-                    </tr>
-                    <tr>
-                        <td><Key text={"P"}/></td>
-                        <td>Cola o texto copiado. O equilvante ao CTRL+V</td>
-                    </tr>
-                    <tr>
-                        <td><Key text={"U"}/></td>
-                        <td>(Undo), o equilvante ao CTRL+Z.</td>
-                    </tr>
-                    <tr>
-                        <td><Key text={"C"}/><Key text={"C"}/></td>
-                        <td>Copia da posição atual até o início da próxima palavra.</td>
                     </tr>
                     <tr>
                         <td><Key text={"Y"}/><Key text={"W"}/></td>
                         <td>Copia da posição atual até o início da próxima palavra.</td>
                     </tr>
+                    <tr>
+                        <td><Key text={"P"}/></td>
+                        <td>Cola o texto copiado ou cortado após o cursor. O equilvante ao CTRL+V</td>
+                    </tr>
+                    <tr>
+                        <td><Key text={"Shift"}/><Key text={"P"}/></td>
+                        <td>Cola o texto copiado ou cortado antes o cursor.</td>
+                    </tr>
+                    <tr>
+                        <td><Key text={"D"}/><Key text={"D"}/></td>
+                        <td>Corta a linha atual (também apaga).</td>
+                    </tr>
                 </table>
                 <h3>Modo de comando</h3>
                 <p>
-                    Acessado com<code className={"code"}>:</code>no Modo Normal, permite que comandos sejam
-                    digitados, como salvar o arquivo<code className={"code"}>:w</code>, sair do editor<code
-                    className={"code"}>:q</code>ou
-                    pesquisar por palavras.
+                    Acessado com <Key text={":"}/> no Modo Normal ou pressionando o <Key text={"ESC"}/> para ir, permite
+                    que comandos sejam
+                    digitados, como salvar o arquivo <Key text={":"}/><Key text={"W"}/>, sair do editor <Key
+                    text={":"}/><Key text={"Q"}/>ou pesquisar por palavras.
+                    Esse modo fornece uma linha de comando na parte inferior da tela, por onde é inserido os comandos a
+                    ser executado. Aqui estão alguns dos
+                    comandos mais úteis e comumente usados no Modo de Comando:
                 </p>
+                <table>
+                    <tr>
+                        <th>Comando</th>
+                        <th>Descrição</th>
+                    </tr>
+                    <tr>
+                        <td><Key text={":"}/><Key text={"W"}/></td>
+                        <td>Salva o arquivo atual.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={":"}/><Key text={"W"}/> + <i>nome_arquivo</i>
+                        </td>
+                        <td>Salva o arquivo atual com um nome diferente.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={":"}/><Key text={"Q"}/>
+                        </td>
+                        <td>Sai do Vim.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={":"}/><Key text={"Q"}/><Key text={"!"}/>
+                        </td>
+                        <td>Sai do Vim sem salvar alterações.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={":"}/><Key text={"W"}/><Key text={"Q"}/>
+                        </td>
+                        <td>Salva o arquivo e sai do Vim.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={":"}/><Key text={"X"}/>
+                        </td>
+                        <td>Salva o arquivo e sai do Vim. (similiar a <code className={"code"}>:wq</code>)</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={":"}/><Key text={"E"}/> + <i>nome_do_arquivo</i>
+                        </td>
+                        <td>Abre um arquivo diferente para edição.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={":"}/><Key text={"E"}/> + <i>nome_do_arquivo</i>
+                        </td>
+                        <td>Abre um arquivo diferente para edição.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={":"}/><Key text={"L"}/><Key text={"S"}/>
+                        </td>
+                        <td>Lista todos os buffers abertos.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={":"}/><Key text={"!"}/> + <i>comando</i>
+                        </td>
+                        <td>Executa um comando do sistema externo (por exemplo, :!ls para listar arquivos).</td>
+                    </tr>
+                </table>
                 <h3>Modo de inserção</h3>
                 <p>
-                    Usado para inserir ou editar o conteúdo do texto. Entrar neste modo é feito com o comando i, e para
-                    sair, pressiona-se ESC.
+                    Usado para inserir ou editar o conteúdo do texto, de maneira semelhante ao modo de
+                    edição em um editor de texto comum. Uma vez no Modo de Inserção, você pode digitar livremente.
+                    Tudo o que você escreve será inserido no texto. As teclas são interpretadas como caracteres a
+                    serem inseridos no buffer de texto, em vez de comandos, como é feito nos modos <b>normal</b>
+                    e <b>comando</b>. Há muitas maneiras de começar no <b>modo de inserção</b> e uma das mais comuns
+                    é feito com a tecla <Key text={"I"}/>, e para sair, pressiona-se <Key text={"ESC"}/>.
                 </p>
+                <h4>Maneiras de entrar no modo de inserção no Vim</h4>
+                <p>
+                    A partir do Modo Normal, você pode entrar no Modo de Inserção com um dos seguintes comandos:
+                </p>
+                <table>
+                    <tr>
+                        <th>Comando</th>
+                        <th>Descrição</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"i"}/>
+                        </td>
+                        <td>Insere texto antes do cursor.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"I"}/>
+                        </td>
+                        <td>Insere texto no início da linha atual.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"a"}/>
+                        </td>
+                        <td>Insere texto após o cursor.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"A"}/>
+                        </td>
+                        <td>Insere texto no final da linha atual.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"o"}/>
+                        </td>
+                        <td>Abre uma nova linha abaixo da linha atual e entra no Modo de Inserção.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"O"}/>
+                        </td>
+                        <td>Abre uma nova linha acima da linha atual e entra no Modo de Inserção.</td>
+                    </tr>
+                </table>
                 <h3>Modo visual</h3>
                 <p>
-                    Para selecionar blocos de texto, permitindo manipulação mais fácil de partes específicas. Pode ser
-                    usado para copiar, colar ou apagar.
+                    Para selecionar e destacar blocos de texto, permitindo manipulação mais fácil de partes específicas.
+                    Pode ser usado para copiar, colar ou apagar. Útil em situações de edição em trechos de texto,
+                    pois permite aplicar em várias linhas de uma só vez!
                 </p>
-
+                <h4>Maneiras de entrar no modo de visual no Vim</h4>
+                <p>
+                    A partir do Modo Normal, você pode entrar no Modo de visual com um dos seguintes comandos:
+                </p>
+                <table>
+                    <tr>
+                        <th>Comando</th>
+                        <th>Descrição</th>
+                        <th>terminal</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"v"}/>
+                        </td>
+                        <td>Entra no <b>Modo Visual</b> e permite selecionar o texto caractere por caractere.</td>
+                        <td><VisualModeTerminal modetext={"VISUAL/LINHA"}/></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"V"}/>
+                        </td>
+                        <td>Entra no <b>Modo Visual em Linha</b> e seleciona linhas inteiras.</td>
+                        <td><VisualModeTerminal modetext={"VISUAL"}/></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"Ctrl"}/>
+                            <Key text={"v"}/>
+                        </td>
+                        <td>Entra no <b>Modo Visual em Bloco</b>, permitindo selecionar blocos
+                            retangulares de texto.
+                        </td>
+                        <td><VisualModeTerminal modetext={"VISUAL/BLOCO"}/></td>
+                    </tr>
+                </table>
+                <p>
+                    Após selecionar o texto, você pode aplicar os seguintes comandos comuns para manipulá-lo:
+                </p>
+                <table>
+                    <tr>
+                        <th>Comando</th>
+                        <th>Descrição</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"d"}/>
+                        </td>
+                        <td>Corta o texto selecionado.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"y"}/>
+                        </td>
+                        <td>Copia o texto selecionado.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"c"}/>
+                        </td>
+                        <td>Corta o texto selecionado e entra no Modo de Inserção para substituir o conteúdo.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"x"}/>
+                        </td>
+                        <td>Apaga o texto selecionado.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"p"}/>
+                        </td>
+                        <td>Cola o conteúdo previamente copiado ou cortado.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"u"}/>
+                        </td>
+                        <td>Desfaz a última alteração.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"U"}/>
+                        </td>
+                        <td>Refaz a alteração desfeita.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={">"}/>
+                        </td>
+                        <td>Indenta a seleção para a direita.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Key text={"<"}/>
+                        </td>
+                        <td>Indenta a seleção para a esquerda.</td>
+                    </tr>
+                </table>
             </div>
         </>
     );
---
+++ src/components/utils/PromptCard/Custom/VisualModeTerminal.jsx
@@ -0,0 +1,72 @@
+import "../PromptCard.css"
+import {useEffect, useRef} from "react";
+import {motion, useAnimation, useInView} from "framer-motion";
+
+const boxVariant = {
+    visible: { opacity: 1, scale: 1 },
+    hidden: { opacity: 0, scale: 0 },
+}
+
+export const VisualModeTerminal = ({modetext}) => {
+
+    const control = useAnimation();
+    const ref = useRef(null)
+    const inView = useInView(ref);
+
+    useEffect(() => {
+        if (inView) {
+            control.start("visible");
+        }else{
+            control.start("hidden");
+        }
+    }, [control, inView]);
+
+    return (
+      <>
+          <motion.div className="card small"
+            ref={ref}
+            variants={boxVariant}
+            initial="hidden"
+            animate={control}
+          >
+              <div className="top">
+                  <p className="title">Terminal</p>
+                  <span className="buttons-prompt">
+                    <button className="minimize button">
+                        <svg viewBox="0 0 10.2 1" y="0px" x="0px"><rect height="1" width="10.2" y="50%"
+                                                                        x="0"></rect></svg>
+                    </button>
+                    <button className="maximize button">
+                        <svg viewBox="0 0 10 10"><path d="M0,0v10h10V0H0z M9,9H1V1h8V9z"></path></svg>
+                    </button>
+                    <button className="close button" onClick={close}>
+                        <svg viewBox="0 0 10 10"><polygon
+                            points="10.2,0.7 9.5,0 5.1,4.4 0.7,0 0,0.7 4.4,5.1 0,9.5 0.7,10.2 5.1,5.8 9.5,10.2 10.2,9.5 5.8,5.1"></polygon></svg>
+                    </button>
+                </span>
+              </div>
+              <div className="topfix">
+              </div>
+              <hr/>
+              <div className="content-prompt">
+               <pre style={{
+                   margin: 0,
+                   display: "flex",
+                   flexDirection: "column",
+                   backgroundColor: "#1C1C1C",
+                   alignItems: "start"
+               }}>
+                  <span style={{color: "#4E4E4E"}}>1</span>
+                  <span style={{color: "#4E4E4E"}}>~</span>
+                  <span style={{color: "#4E4E4E"}}>~</span>
+                  <span style={{color: "#4E4E4E"}}>~</span>
+                  <span style={{color: "#4E4E4E"}}>~</span>
+                  <span style={{color: "#4E4E4E"}}>~</span>
+                  <span style={{color: "#4E4E4E"}}>~</span>
+                  <span style={{color: "#4E4E4E"}}>-- {modetext} --</span>
+              </pre>
+              </div>
+          </motion.div>
+      </>
+    );
+}
